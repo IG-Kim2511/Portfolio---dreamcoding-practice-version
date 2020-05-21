5b44699c27a1a48a722038d589a7ddcdfe0fb570
IanKim <59094101+IanKim@users.noreply.github.com>
2020-05-21 18:32:16 -0400

    JS project버튼누르면 해당project만 나오게 하기

--- main.js
+++ main.js
@@ -94,3 +94,43 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+// projects - project list클릭하면, 해당 프로젝트만 뜨게 하기 . html data 사용
+
+// 알고리즘
+// 1. 버튼. 컨테이너 . 프로젝트 가져옴
+// 2. click, addEventListener
+// 3. button, span 의 data  가져오기
+// 4. forEach로 dataset목록 다 돌리고, 해당되는 아이템만 보이게하기
+
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+// e: event
+// event안에 있는
+// target 안에 있는
+// dataset안에 있는
+// filter값 프린트하기
+workBtnContainer.addEventListener("click", (e) => {
+  // debugging하기 : breakpoint :코드넘버 누르면 breakpoint생김
+  // source-page-main.js - ( scope- e ) - watch - e.target- e.target.parentNode- e.target.parentNode.dataset.filter 검색해보기
+
+  // 3. button, span 의 data  가져오기
+  // dataset: html 의 data 가져오는 코드
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  // 만약을 위해 코드 추가 if...filter가 null이면 return..끝냄
+  if (filter == null) {
+    return;
+  }
+
+  // 4. forEach로 dataset목록 다 돌리고, 해당되는 아이템만 보이게하기
+  projects.forEach((project) => {
+    console.log(project.dataset.type);
+    if (filter === "*" || filter === project.dataset.type) {
+      project.classList.remove("invisible");
+    } else {
+      project.classList.add("invisible");
+    }
+  });
+});
